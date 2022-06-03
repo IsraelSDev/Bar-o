b52986f92049cb779b340084c2345622e8c81b4a
Israel S. Santos <israel.santos@zukk.com.br>
2022-06-03 19:37:23 -0300

    ajustado questões

--- js/survey.js
+++ js/survey.js
@@ -1,82 +1,74 @@
-//Js obfuscated to avoid copy
-
-jQuery(function (_0x5779x1) {
-    'use strict';
-    _0x5779x1('form#wrapped')['attr']('action', 'save_data.php');
-    _0x5779x1('#wizard_container')['wizard']({
-        stepsWrapper: '#wrapped',
-        submit: '.submit',
-        beforeSelect: function (_0x5779x4, _0x5779x5) {
-            if (_0x5779x1('input#website')['val']()['length'] != 0) {
-                return false
-            };
-            if (!_0x5779x5['isMovingForward']) {
-                return true
-            };
-            var _0x5779x6 = _0x5779x1(this)['wizard']('state')['step']['find'](':input');
-            return !_0x5779x6['length'] || !!_0x5779x6['valid']()
+"use strict";
+jQuery(function ($) {
+  $("form#wrapped").attr("action", "save_data.php");
+  $("#wizard_container")
+    .wizard({
+      stepsWrapper: "#wrapped",
+      submit: ".submit",
+      beforeSelect: function parse(shortCircuit, _parseFn) {
+        if ($("input#website").val().length != 0) {
+          return false;
         }
-    })['validate']({
-        errorPlacement: function (_0x5779x2, _0x5779x3) {
-            if (_0x5779x3['is'](':radio') || _0x5779x3['is'](':checkbox')) {
-                _0x5779x2['insertBefore'](_0x5779x3['next']())
-            } else {
-                _0x5779x2['insertAfter'](_0x5779x3)
-            }
+        if (!_parseFn.isMovingForward) {
+          return true;
         }
-    });
-    _0x5779x1('#progressbar')['progressbar']();
-    _0x5779x1('#wizard_container')['wizard']({
-        afterSelect: function (_0x5779x4, _0x5779x5) {
-            _0x5779x1('#progressbar')['progressbar']('value', _0x5779x5['percentComplete']);
-            _0x5779x1('#location')['text']('(' + _0x5779x5['stepsComplete'] + '/' + _0x5779x5['stepsPossible'] + ')')
-        }
-    });
-    _0x5779x1('#wrapped')['validate']({
-        ignore: [],
-        rules: {
-            select: {
-                required: true
-            }
-        },
-        errorPlacement: function (_0x5779x2, _0x5779x3) {
-            if (_0x5779x3['is']('select:hidden')) {
-                _0x5779x2['insertAfter'](_0x5779x3['next']('.nice-select'))
-            } else {
-                _0x5779x2['insertAfter'](_0x5779x3)
-            }
-        }
+        var params = $(this).wizard("state").step.find(":input");
+        return !params.length || !!params.valid();
+      },
     })
-
+    .validate({
+      errorPlacement: function mergeChildren(parent, node) {
+        if (node.is(":radio") || node.is(":checkbox")) {
+          parent.insertBefore(node.next());
+        } else {
+          parent.insertAfter(node);
+        }
+      },
+    });
+  $("#progressbar").progressbar();
+  $("#wizard_container").wizard({
+    afterSelect: function compile(ngAttrs, jqmAttrs) {
+      $("#progressbar").progressbar("value", jqmAttrs.percentComplete);
+      $("#location").text(
+        "(" + jqmAttrs.stepsComplete + "/" + jqmAttrs.stepsPossible + ")"
+      );
+    },
+  });
+  $("#wrapped").validate({
+    ignore: [],
+    rules: { select: { required: true } },
+    errorPlacement: function translate(options, data) {
+      if (data.is("select:hidden")) {
+        options.insertAfter(data.next(".nice-select"));
+      } else {
+        options.insertAfter(data);
+      }
+    },
+  });
 });
-
-function getVals(_0x5779x8, _0x5779x9) {
-    switch (_0x5779x9) {
-        case 'question_1':
-            var _0x5779xa = $(_0x5779x8)['val']();
-            $('#question_1')['text'](_0x5779xa);
-            break;
-        case 'additional_message':
-            var _0x5779xa = $(_0x5779x8)['val']();
-            $('#additional_message')['text'](_0x5779xa);
-            break;
-        case 'question_2':
-            var _0x5779xa = $(_0x5779x8)['val']();
-            $('#question_2')['text'](_0x5779xa);
-            break;
-        case 'additional_message_2':
-            var _0x5779xa = $(_0x5779x8)['val']();
-            $('#additional_message_2')['text'](_0x5779xa);
-            break;
-        case 'question_3':
-            var _0x5779xb = $(_0x5779x8)['attr']('name');
-            var _0x5779xa = [];
-            $('input[name*=\'' + _0x5779xb + '\']')['each'](function () {
-                if (jQuery(this)['is'](':checked')) {
-                    _0x5779xa['push']($(this)['val']())
-                }
-            });
-            $('#question_3')['text'](_0x5779xa['join'](', '));
-            break
-    }
+function getVals(key, filter) {
+  switch (filter) {
+    case "question_1":
+      var value = $(key).val();
+      $("#question_1").text(value);
+      break;
+    case "question_2":
+      value = $(key).val();
+      $("#question_2").text(value);
+      break;
+    case "question_3":
+      var versionByName = $(key).attr("name");
+      value = [];
+      $("input[name*='" + versionByName + "']").each(function () {
+        if (jQuery(this).is(":checked")) {
+          value.push($(this).val());
+        }
+      });
+      $("#question_3").text(value.join(", "));
+      break;
+    case "additional_message_3":
+      value = $(key).val();
+      $("#additional_message_3").text(value);
+      break;
+  }
 }
--- save_data.php
+++ save_data.php
@@ -9,21 +9,14 @@
     $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
     $atendimentoCaixa = $_POST['question_1'];
-    $sujestaoCaixa = $_POST['additional_message'];
+    $esperaFila = $_POST['question_2'];
     $recomendaService = $_POST['question_2'];
-    $sujestaoRecomendaService = $_POST['additional_message_2'];
     $howMeet = implode("|",$_POST['question_3']);
-    $name = $_POST['firstname'];
-    $lastName = $_POST['lastname'];
-    $email = $_POST['email'];
-    $phone = $_POST['telephone'];
-    $age = $_POST['age'];
-    $gender = $_POST['gender'];
-    
-    $data_form = "INSERT INTO userdata(atendimentoCaixa, sujestaoCaixa, recomendaServico, sujestaoRecomendaServico, comoConheceu, nome, sobreNome, email, fone, idade, genero, dataEnvio) VALUES ('$atendimentoCaixa', '$sujestaoCaixa', '$recomendaService', '$sujestaoRecomendaService', '$howMeet', '$name', '$lastName', '$email', '$phone', '$age', '$gender', NOW())";
+      
+    $data_form = "INSERT INTO userdata(atendimentoCaixa, recomendaServico, sujestaoRecomendaServico, comoConheceu,  dataEnvio) VALUES ('$atendimentoCaixa',  '$recomendaService', '$esperaFila', '$howMeet',  NOW())";
 
    mysqli_query($conn, $data_form);
    
    echo  "<script>alert('Email enviado com Sucesso!'); location.href=\"index.html\";</script>";
-    
+
 ?>
